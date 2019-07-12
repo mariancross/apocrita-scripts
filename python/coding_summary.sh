636a916c86032e941635b03cd5091815c97780cf
Maria Santamaria <mcsan_16@outlook.com>
2019-07-12 14:33:14 +0100

    renaming files

--- python/coding_summary.sh
+++ python/coding_summary.sh
@@ -5,11 +5,8 @@
 #$ -l h_rt=1:0:0
 #$ -l h_vmem=8G
 
-# Activate virtualenv
 source ~/pyenv/bin/activate
 
-# Run Python script
 python ~/workspaces/py-tools/src/main.py -t coding_summary -d  $PWD -e **/encLog* -b **/decLog* > results.csv
 
-# Deactivate
 deactivate
