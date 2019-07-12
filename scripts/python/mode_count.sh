4dc6005548ab48870b5d687e600f45975f6cd9b6
Maria Santamaria <mcsan_16@outlook.com>
2019-07-12 11:42:18 +0100

    delete comments and add array job support

--- scripts/python/mode_count.sh
+++ scripts/python/mode_count.sh
@@ -4,13 +4,11 @@
 #$ -pe smp 1
 #$ -l h_rt=1:0:0
 #$ -l h_vmem=8G
+#$ -t 1-18
 
-# Activate virtualenv
 source ~/pyenv/bin/activate
 
-# Run Python script
 INPUT_ARGS=$(sed -n "${SGE_TASK_ID}p" test_sequences.txt)
 python  ~/workspaces/py-tools/src/main.py -t mode_count -d $PWD -b "**/decLog_"$INPUT_ARGS"*.out"
 
-# Deactivate
 deactivate
