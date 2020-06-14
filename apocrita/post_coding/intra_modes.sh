b9737c5d9b02ea00d1aa3916364fff245a1d328f
María Santamaría <mcsan_16@outlook.com>
2020-06-14 21:58:11 +0100

    Adding script to get intra-prediction modes per sequence

--- apocrita/post_coding/intra_modes.sh
+++ apocrita/post_coding/intra_modes.sh
@@ -4,12 +4,10 @@
 #$ -pe smp 1
 #$ -l h_rt=0:15:0
 #$ -l h_vmem=8G
-#$ -t 1-18
 
 # shellcheck disable=SC1090
 . "${HOME}/pyenv/bin/activate"
 
-INPUT_ARGS=$(sed -n "${SGE_TASK_ID}p" test_sequences.txt)
-python "${HOME}/workspaces/phd=scripts/coding" --task intra_modes --dir "${PWD}" --dec_file_rgx "**/decLog_"${INPUT_ARGS}"*.out" --output_file "modes_"${INPUT_ARGS}".xlsx"
+python "${HOME}/workspaces/phd-scripts/coding" --task intra_modes --dir "${PWD}" --dec_file_rgx ./**/decLog* --output_file "modes.xlsx"
 
 deactivate
---
+++ apocrita/post_coding/intra_modes_seq.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#$ -cwd
+#$ -j y
+#$ -pe smp 1
+#$ -l h_rt=0:15:0
+#$ -l h_vmem=8G
+#$ -t 1-18
+
+# shellcheck disable=SC1090
+. "${HOME}/pyenv/bin/activate"
+
+INPUT_ARGS=$(sed -n "${SGE_TASK_ID}p" "${HOME}/workspaces/phd-scripts/resources/hevc_sequences.txt")
+python "${HOME}/workspaces/phd-scripts/coding" --task intra_modes --dir "${PWD}" --dec_file_rgx "./**/decLog_${INPUT_ARGS}*.out" --output_file "modes_${INPUT_ARGS}.xlsx"
+
+deactivate
